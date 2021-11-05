d632e029e2f84d7653d0aaca742d186132dc03dd
Cryptec <neupi92@web.de>
2021-11-05 14:18:45 +0100

    added second screen and handler

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react'
 import generate from './components/getMeals'
 import './App.css';
 
+import { openCookBook, closeCookBook } from './utils/handler'
+import CookBook from './screens/CookBook';
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
         answerDenied: "Denied",
         displayMeal: "",
-        isActive: false
+        isActive: false,
+        nav: true
     };
 }
 
@@ -19,6 +23,20 @@ generateMeal = () => {
    })
 }
 
+toggleOpen = () => {
+  openCookBook()
+  this.setState({nav: true})
+}
+
+toggleClose = () => {
+  closeCookBook()
+  this.setState({ nav: false })
+}
+
+toggleNav = () => {
+  this.state.nav ? this.toggleClose() : this.toggleOpen();
+}
+
   render() {
 
     let displayMeal = this.state.displayMeal
@@ -27,7 +45,8 @@ generateMeal = () => {
 
     <div className="App">
       <div className="Home">
-
+      <CookBook />
+      <div className="AddButton" onClick={this.toggleNav}>+</div>
       <div className="displayContainer">
       {this.state.isActive ? <p className="displayText">{displayMeal}</p>  : null }
       </div>
--- Frontend/src/utils/handler.js
+++ Frontend/src/utils/handler.js
@@ -1,11 +1,7 @@
 export function openCookBook() {
-    document.getElementById("cookbook").style.width = "100";
-    document.getElementById("topbar").style.marginLeft = "var(--sidebar-width)";
-    document.getElementById("contentpage").style.marginLeft = "var(--sidebar-width)";
+    document.getElementById("cookbook").style.width = "0";
 }
 
 export function closeCookBook() {
-    document.getElementById("cookbook").style.width = "0";
-    document.getElementById("topbar").style.marginLeft = "0";
-    document.getElementById("contentpage").style.marginLeft = "0";
+    document.getElementById("cookbook").style.width = "100%";
 }
